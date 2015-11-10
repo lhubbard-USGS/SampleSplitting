2162c987aaf8a68db7c06a035eb2b7b1297bf03d
Jessica Thompson <jlthomps@usgs.gov>
2015-11-10 10:22:27 -0600

    more edits for MI/IN

---
+++ R/labVolumesTableMI.R
@@ -0,0 +1,30 @@
+#' Function to generate clean text file of sample amounts for lab
+#' 
+#' This function accepts a vector of storm names, vector of storm start and end datetimes and list of dataframes containing event data
+#' 
+#' @param StormName vector of storm name(s)
+#' @param StormStart vector of storm start dates
+#' @param StormEnd vector of storm end dates
+#' @param tableOut list of data frames containing event data
+#' @param bottlePickup dates bottles were retrieved
+#' @export
+labVolumesTableMI <- function(StormName,StormStart,StormEnd,tableOut,bottlePickup){
+  fileName <- paste(StormName[1],"labVolumes",".txt",sep="")
+  less <- 0
+  sink(fileName)
+  for (i in 1:length(StormName)) {
+    if (!is.na(tableOut[[i]][1,2])) {
+    cat("==================================================================================","\n")
+    cat("\t",StormName[i],"\t\t",strftime(StormStart[i],format="%Y-%m-%d %H:%M"),"\t",strftime(StormEnd[i],format="%Y-%m-%d %H:%M"),"\n")
+    cat("==================================================================================","\n")
+    labTable <- tableOut[[i]]
+    for (j in 1:nrow(labTable)) {
+      cat("\t",paste0(labTable$subNum[j],paste0(rep(" ",max(nchar(labTable$subNum))),collapse=""),collapse=""),"\t",strftime(labTable$datetime[j],format="%Y-%m-%d %H:%M",tz="UTC"),"\t",labTable$mL[j],"\n")
+    }
+    cat("==================================================================================","\n")
+    cat("\t",bottlePickup[i-less],"\n")
+    cat("==================================================================================","\n")
+    } else {less<-less+1}
+  }
+  sink()
+}
--- inst/SampleSplitWorkflowMI.R
+++ inst/SampleSplitWorkflowMI.R
@@ -2,7 +2,6 @@ install.packages(c("googleVis"), dependencies=TRUE)
 install.packages(c("dataRetrieval"), repos="http://usgs-r.github.com")
 install.packages("devtools")
 install.packages(c("SampleSplitting"), repos="http://usgs-r.github.com")
-# install_github("jlthomps/SampleSplitting")
 install.packages(c("USGSwsData", "USGSwsBase", "USGSwsGraphs", "USGSwsStats", "USGSwsQW"), repos="http://usgs-r.github.com")
 install.packages(c("XML", "digest", "memoise", "lubridate", "akima", "leaps", "car", "mvtnorm", "relimp", "BSDA", "RODBC"), dependencies=TRUE)
 ########################################################################################################
@@ -11,27 +10,26 @@ library(dataRetrieval)
 library(googleVis)
 
 #EDIT to include the name of the new storm folder
-setwd("Q:/crachol/GLRI Edge of Field Study/Sample selection and splits/Alger Creek Basin/2015/MI-TL1/MI-TL1-61/TEST")
+setwd("//Sunset/data s archives/HNS_Archive/Water_Quality/GLRI_Edge_of_Field/RStudio Files")
 
 # enter NWIS station id for gaging station
-siteNo <- "0414826545"
+siteNo <- "411229084541102"
 # enter date to begin pulling data (rounded to the day)
-StartDt <- '2015-06-07'
+StartDt <- '2015-10-28'
 # enter date to stop pulling data (rounded to the day)
-EndDt <- '2015-06-09'
+EndDt <- '2015-10-28'
 # enter NWIS station id for precipitation gaging station, may or may not be identical to "siteNo"
-precipSite <- "0414826545"
+precipSite <- "411228084541701"
 #tzCode <- "America/New_York"
 
-# enter path and name of data file if data is not web-available
-dataFile <- "M:/NonPoint Evaluation/GLRI Edge-of-field/Splitting Record Conversion to R/PLAT2TESTJLT.RDB"
+# enter path and name of data file if data is not web-available - will need to be output to RDB in UTC time
+#dataFile <- "//Sunset/data s archives/HNS_Archive/Water_Quality/GLRI_Edge_of_Field/RStudio Files/TL2.RDB"
 
 # Run ONLY 1 of these options, depending on whether you are pulling data from the web, or providing a file
 # Retrieve data from NWISWeb (if available)
 adaps_data_all <- getADAPSData(siteNo,StartDt,EndDt,precipSite)
-#attr(adaps_data_all$datetime,"tzone") <- "EST"
 # or use file names to pull data in from files exported by ADAPS
-adaps_data_all <- getADAPSData(siteNo,StartDt,EndDt,precipSite,dataFile)
+#adaps_data_all <- getADAPSData(siteNo,StartDt,EndDt,precipSite,dataFile)
 
 # save merged data for station/storm event, saved as file, eg 434425090462401data.txt 
 adaps_data_allLocal <- adaps_data_all
@@ -48,23 +46,24 @@ hydrographPDF(adaps_data_allLocal,siteNo,dateInt=8)
 
 # IF you have un-sampled storms, you may enter their StormStart and StormEnd values, as well as StormNames in 
 # the appropriate list. Leave them out of the maxBottleVol, maxSampVol and subNum lists
+# times should be in local/tz used in NWIS (not UTC)
 # enter the name of the storm(s) (for plot title)
-StormName <- c("MITL-61")
+StormName <- c("411229084541102October28")
 # enter Storm Start date(s)
 # MUST be in the format YYYY-MM-DD HH:24
-StormStart <- c("2015-06-08 03:30")
+StormStart <- c("2015-10-28 01:15")
 # enter Storm End date(s) 
 # MUST be in the format YYYY-MM-DD HH:24
-StormEnd <- c("2015-06-08 16:30")
+StormEnd <- c("2015-10-28 14:45")
 # enter the maximum possible volume for one sample bottle
 maxBottleVol <- c(800)
 # enter the maximum possible volume for one full storm sample
 maxSampVol <- c(3900)
 # enter number for 1st bottle of each storm, if a number other than 1 is desired
-subNum <- c(2125)
+subNum <- c(1001)
 
 # generate bottle volume table(s) for lab for each storm
-source("C:/Users/jlthomps/Desktop/git/SampleSplitting/R/labDataOutMI.R")
+source("//Sunset/data s archives/HNS_Archive/Water_Quality/GLRI_Edge_of_Field/RStudio Files/labDataOutMI.R")
 tableOut <- labDataOutMI(adaps_data_all,StormStart,StormEnd,StormName,maxBottleVol,maxSampVol,subNum=subNum)
 # look at table(s) generated for lab sample instructions for storm event(s). determine if changes are needed
 for (i in 1:length(StormStart)){
@@ -76,7 +75,7 @@ intermediateVolTable(siteNo,StormStart,StormEnd,tableOut)
 #######################################################################################################
 # OPTIONAL if sample(s) need to be removed, enter their datetime and a comment and re-create tableOut
 # MUST be in the format YYYY-MM-DD HH:24
-removeDate <- c("2015-03-10 15:40:00","2015-03-10 16:55:00","2015-03-12 14:10:00","2015-03-12 14:05:00")
+removeDate <- c("")
 removeComment <- c("")
 tableOut <- labDataOutMI(adaps_data_all,StormStart,StormEnd,StormName,maxBottleVol,maxSampVol,removeDate=removeDate,subNum=subNum)
 for (i in 1:length(StormStart)){
@@ -87,9 +86,10 @@ intermediateVolTable(siteNo,StormStart,StormEnd,tableOut)
 
 #Once you are satisfied with the table output
 #enter date(s) when samples were picked up 
-bottlePickup <- c("Bottles MI-TL1-2125 through MI-TL1-2127 were shipped on 2015-06-08.") 
+bottlePickup <- c("Bottles 1001 through 1007 picked up 2015-10-29") 
 # generate text file with storm event sample bottle volme table(s)
 stormEventsTable(StormName,StormStart,StormEnd,tableOut,maxBottleVol,bottlePickup)
 
 # generate simple table for lab
-labVolumesTable(StormName,StormStart,StormEnd,tableOut,bottlePickup)
+source("//Sunset/data s archives/HNS_Archive/Water_Quality/GLRI_Edge_of_Field/RStudio Files/labVolumesTableMI.R")
+labVolumesTableMI(StormName,StormStart,StormEnd,tableOut,bottlePickup)

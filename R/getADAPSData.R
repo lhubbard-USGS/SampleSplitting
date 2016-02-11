1fa427c39264126c7b47802f38e1b32fc1f6e0ba
Laura DeCicco <ldecicco@usgs.gov>
2016-02-11 14:27:38 -0600

    Final roxygen update

--- R/getADAPSData.R
+++ R/getADAPSData.R
@@ -92,3 +92,8 @@ if ((length(unique(POR$parameter_cd)))+(length(unique(PORprecip$parameter_cd)))>
         PORAll <- rbind(POR,PORprecip)
         print(PORAll[,c("parameter_cd","startDate","endDate","count","parameter_nm")],row.names=FALSE)} 
 }}
+
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Although this software program has been used by the U.S. Geological Survey (USGS), no warranty, expressed or implied, is made by the USGS or the U.S. Government as to the accuracy and functioning of the program and related program material nor shall the fact of distribution constitute any such warranty, and no responsibility is assumed by the USGS in connection therewith.")
+}
